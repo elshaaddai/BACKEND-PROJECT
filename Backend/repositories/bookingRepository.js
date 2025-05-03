90166c0567a29f121818f21dd3e58ea7ccda5780
piyaya06 <monoapril7@gmail.com>
2025-05-04 02:33:56 +0800

    Adding Fetch Layer: BookingRepository

---
+++ Backend/repositories/bookingRepository.js
@@ -0,0 +1,28 @@
+const User = require("../models/Users");
+
+const createBooking = async (email, bookingData) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  user.booking.push(bookingData);
+  return await user.save();
+};
+
+const findBookings = async () => {
+  const users = await User.find({}, "name email booking");
+  return users.flatMap((user) =>
+    user.booking.map((booking) => ({
+      userId: user._id,
+      name: user.name,
+      email: user.email,
+      dateTime: booking.dateTime,
+      additionalDetails: booking.additionalDetails,
+    }))
+  );
+};
+
+module.exports = {
+  createBooking,
+  findBookings,
+};
