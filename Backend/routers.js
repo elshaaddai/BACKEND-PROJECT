17bf3f73c882d7ee2fe6023a9337b18a61339c25
fdricck <s22210431@student.unklab.ac.id>
2025-05-04 02:54:11 +0800

    updating routers

--- Backend/routers.js
+++ Backend/routers.js
@@ -2,4 +2,18 @@ const express = require("express");
 const router = express.Router();
 require("./mongoose");
 
+// Import controllers
+const userController = require("./controllers/userController");
+const bookingController = require("./controllers/bookingController");
+
+// User routes
+router.get("/Users", userController.getUsers);
+router.post("/Users", userController.createUser);
+router.put("/Users/:id", userController.updateUser);
+router.delete("/Users/:id", userController.deleteUser);
+
+// Booking routes
+router.get("/booking", bookingController.getBookings);
+router.post("/booking", bookingController.createBooking);
+
 module.exports = router;
